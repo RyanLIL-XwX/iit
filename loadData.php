f0000e5eab898d80ee98a68f983701fe70ef1ffb
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2021-12-06 19:39:00 -0500

    Add files via upload

---
+++ loadData.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<head>
+   <title>My project page</title>
+</head>
+
+<body>
+
+   <h1>For final quiz</h1>
+
+   <p>
+      <li><a href="../Lab2/ITWS_Han_Zhen_Qin_lab2.html" target="_blank">lab2</a></li><br />
+      <a href="../Lab3/HanZhen_Qin_DATA.html" target="_blank">lab3 -> project_page</a><br />
+      <a href="../Lab3/HanZhen_Qin_DATA.html" target="_blank">lab3 -> main_page</a><br />
+      <a href="../Lab4/HanZhen_Qin_lab4.html" target="_blank">lab4 -> project_page</a><br />
+      <a href="../Lab5/lab5.html" target="_blank">lab5 (HTML)</a><br />
+      <a href="../Lab5/lab5.css" target="_blank">lab5 (CSS)</a><br />
+      <a href="../Lab5/lab5.js" target="_blank">lab5 (Javascript)</a><br />
+      <a href="../Lab6/lab6.html" target="_blank">lab6 (HTML)</a><br />
+      <a href="../Lab6/lab6.js" target="_blank">lab6 (Javascript)</a><br />
+      <a href="../Lab6/lab6.css" target="_blank">lab6 (CSS)</a><br />
+      <a href="../Lab8/lab8.html" target="_blank">lab8 (HTML)</a><br />
+      <a href="../Lab8/lab8.css" target="_blank">lab8 (CSS)</a><br />
+      <a href="../Lab8/lab8.js" target="_blank">lab8 (Javascript)</a><br />
+      <a href="../Lab9/HanZhen_Qin_homepage.html" target="_blank">lab9 (HTML)</a><br />
+      <a href="../Lab9/homepage.js" target="_blank">lab9 (Javascript)</a><br />
+      <a href="../Lab9/lab9.js" target="_blank">lab9 (javascript)</a><br />
+      <a href="load.php" target="_blank">import</a>
+   </p>
+
+</body>
